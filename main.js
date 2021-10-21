489bd26abfcb5e66dcf2058449103208f1eed061
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-10-21 16:37:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 //main.js
+var music = "";
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.position(440, 160);
+
+    video = createCapture(400, 400);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 400, 400);
+}
+
+function play(){
+    music.play();
+}
