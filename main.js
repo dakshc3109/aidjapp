7f68520a40a8711914e03b2c2196d86b8de63f3f
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-10-26 17:33:02 +0530

    //adeed js code

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-//main.js
+var leftWristX = 0;
+var leftWristY = 0;
+
+var rightWristX = 0;
+var rightWristY = 0;
+
 var music = "";
 function preload(){
     music = loadSound("music.mp3");
@@ -8,14 +13,34 @@ function setup(){
     canvas = createCanvas(400, 400);
     canvas.position(440, 160);
 
-    video = createCapture(400, 400);
+    video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 400, 400);
 }
 
+function modelLoaded(){
+    console.log("pose net is intialized");
+}
+
 function play(){
     music.play();
+    music.setVolume(1);
+    music.rate(1);
+}
+
+function gotPoses(results){
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("left wrist x "+leftWristX+" leftWristY "+leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("right wrist x "+rightWristX+" rightWristY "+rightWristY);
 }
