54ce40a17d205ccec185a6719d48812ba9ddc89d
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-10-28 18:01:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var leftWristScore = 0;
 var leftWristX = 0;
 var leftWristY = 0;
 
@@ -10,7 +11,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(400, 400);
+    canvas = createCanvas(600, 500);
     canvas.position(440, 160);
 
     video = createCapture(VIDEO);
@@ -21,7 +22,23 @@ function setup(){
 }
 
 function draw(){
-    image(video, 0, 0, 400, 400);
+    image(video, 0, 0, 600, 500);
+
+
+    fill("#FF0000");
+    stroke("FF0000");
+    if(leftWristScore > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    inNumberLeftWristY = Number(leftWristY);
+    withoutDecimalLeftWristY = floor(inNumberLeftWristY);
+    console.log(withoutDecimalLeftWristY);
+    //divide = withoutDecimalLeftWristY/1000;
+    //volume = divide*2;
+    volume = withoutDecimalLeftWristY/600;
+    console.log("volume : "+volume);
+    document.getElementById("volume").innerHTML = "Volume: "+volume;
+    music.setVolume(volume);
+    }
 }
 
 function modelLoaded(){
@@ -35,6 +52,7 @@ function play(){
 }
 
 function gotPoses(results){
+    if(results.length > 0){
     console.log(results);
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
@@ -43,4 +61,8 @@ function gotPoses(results){
     rightWristX = results[0].pose.rightWrist.x;
     rightWristY = results[0].pose.rightWrist.y;
     console.log("right wrist x "+rightWristX+" rightWristY "+rightWristY);
+
+    leftWristScore = results[0].pose.keypoints[9].score;
+    console.log(leftWristScore)
+    }
 }
