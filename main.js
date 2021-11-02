02edfa16fc3fe7e71527c46d32bcdc2e38266e49
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-11-02 17:55:33 +0530

    //added js for speed and volume

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+var music = "";
 var leftWristScore = 0;
+var rightWristScore = 0;
 var leftWristX = 0;
 var leftWristY = 0;
 
 var rightWristX = 0;
 var rightWristY = 0;
 
-var music = "";
+
 function preload(){
     music = loadSound("music.mp3");
 }
 
 function setup(){
     canvas = createCanvas(600, 500);
-    canvas.position(440, 160);
+    //canvas.position(440, 160);
+    canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
@@ -21,48 +24,82 @@ function setup(){
     posenet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
+function modelLoaded(){
+    console.log("poseNet is intialized");
+}
 
-
-    fill("#FF0000");
-    stroke("FF0000");
-    if(leftWristScore > 0.2){
-    circle(leftWristX, leftWristY, 20);
-    inNumberLeftWristY = Number(leftWristY);
-    withoutDecimalLeftWristY = floor(inNumberLeftWristY);
-    console.log(withoutDecimalLeftWristY);
-    //divide = withoutDecimalLeftWristY/1000;
-    //volume = divide*2;
-    volume = withoutDecimalLeftWristY/600;
-    console.log("volume : "+volume);
-    document.getElementById("volume").innerHTML = "Volume: "+volume;
-    music.setVolume(volume);
+function gotPoses(results){
+    if(results.length > 0){
+	    console.log(results);
+	    leftWristX = results[0].pose.leftWrist.x;
+	    leftWristY = results[0].pose.leftWrist.y;
+	    console.log("left wrist x "+leftWristX+" leftWristY "+leftWristY);
+	
+	    rightWristX = results[0].pose.rightWrist.x;
+	    rightWristY = results[0].pose.rightWrist.y;
+	    console.log("right wrist x "+rightWristX+" rightWristY "+rightWristY);
+	
+	    leftWristScore = results[0].pose.keypoints[9].score*1000;
+	    rightWristScore = results[0].pose.keypoints[10].score*100;
+	    console.log(leftWristScore);
+		console.log(rightWristScore);
     }
 }
 
-function modelLoaded(){
-    console.log("pose net is intialized");
+function draw(){
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("FF0000");
+	//circle(552.5809001364903, 539.4442585430182, 20);
+	console.log(rightWristScore); 
+	if(rightWristScore > 0.2){
+		circle(rightWristX, rightWristY, 20);
+		if(rightWristY > 0 && rightWristY<=100){
+			document.getElementById("speed").innerHTML = "Speed: 0.5";
+			music.rate(0.5);
+		}
+
+		else if(rightWristY > 100 && rightWristY<=200){
+			document.getElementById("speed").innerHTML = "Speed: 1";
+			music.rate(1);
+		}
+
+		else if(rightWristY > 200 && rightWristY<=300){
+			document.getElementById("speed").innerHTML = "Speed: 1.5";
+			music.rate(1.5);
+		}
+
+		else if(rightWristY > 300 && rightWristY<=400){
+			document.getElementById("speed").innerHTML = "Speed: 2";
+			music.rate(2);
+		}
+
+		else if(rightWristY > 400 && rightWristY<=500){
+			document.getElementById("speed").innerHTML = "Speed: 2.5";
+			music.rate(2.5);
+		}
+	}
+
+    if(leftWristScore > 0.2){
+	    circle(leftWristX, leftWristY, 20);
+	    
+	    inNumberLeftWristY = Number(leftWristY);
+	    withoutDecimalLeftWristY = floor(inNumberLeftWristY);
+	    console.log(withoutDecimalLeftWristY);
+	    //divide = withoutDecimalLeftWristY/1000;
+	    //volume = divide*2;
+	    volume = withoutDecimalLeftWristY/500;
+	    console.log("volume : "+volume);
+	    document.getElementById("volume").innerHTML = "Volume: "+volume;
+	    music.setVolume(volume);
+	}
 }
+
+
 
 function play(){
     music.play();
     music.setVolume(1);
     music.rate(1);
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-    console.log(results);
-    leftWristX = results[0].pose.leftWrist.x;
-    leftWristY = results[0].pose.leftWrist.y;
-    console.log("left wrist x "+leftWristX+" leftWristY "+leftWristY);
-
-    rightWristX = results[0].pose.rightWrist.x;
-    rightWristY = results[0].pose.rightWrist.y;
-    console.log("right wrist x "+rightWristX+" rightWristY "+rightWristY);
-
-    leftWristScore = results[0].pose.keypoints[9].score;
-    console.log(leftWristScore)
-    }
 }
